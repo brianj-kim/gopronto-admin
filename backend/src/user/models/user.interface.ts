dc2c6a09b9d2f9147117fdce6dd8e8b2c426c541
Jiwoun Kim <jiwoone@gmail.com>
2022-05-11 14:03:41 -0600

    Add blog.module and blog-entry.entity blog-entry.interface as well as the ManyToOne relationship to the user

--- backend/src/user/models/user.interface.ts
+++ backend/src/user/models/user.interface.ts
@@ -1,3 +1,5 @@
+import { BlogEntry } from 'src/blog/model/blog-netry.interface';
+
 export interface User {
   id?: number;
   name?: string;
@@ -6,6 +8,7 @@ export interface User {
   password?: string;
   role?: UserRole;
   profileImage?: string;
+  blogEntries?: BlogEntry[];
 }
 
 export enum UserRole {
